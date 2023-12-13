e0f61689a662d6d5ab8289ef128ab253f3d183e9
mankcom3799@hufs.ac.kr <mankcom3799@hufs.ac.krecho>
2023-12-13 16:45:34 +0900

    feat #2 : controller uuid 추가

--- src/main/java/com/pjh/s3imageupload/Controller/S3FileUploadController.java
+++ src/main/java/com/pjh/s3imageupload/Controller/S3FileUploadController.java
@@ -1,5 +1,6 @@
 package com.pjh.s3imageupload.Controller;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,29 +12,35 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/upload")
 public class S3FileUploadController {
 
-    private final AmazonS3Client amazonS3Client;
+    private final AmazonS3 amazonS3;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public S3FileUploadController(AmazonS3Client amazonS3Client){
-        this.amazonS3Client = amazonS3Client;
+    public S3FileUploadController(AmazonS3 amazonS3){
+        this.amazonS3 = amazonS3;
     }
 
     @PostMapping
     public ResponseEntity<?> uploadFile(@RequestParam("file")MultipartFile file){
         try {
             String fileOriginalName = file.getOriginalFilename();
-            // url = "http://pjhaws3uploadImage/test/myfile.png"
+            // 파일을 중복해서 올리면 s3에 올라가지 않음, 그러므로 uuid를 추가해서
+            // 해당 이미지 파일을 엄청 낮은 확률이 아닌이상 중복을 제거
+            UUID uuid = UUID.randomUUID();
+            String uuidFileName = uuid+"_"+fileOriginalName;
+
             String url = "http://"+bucket+"/test"+fileOriginalName;
             ObjectMetadata metaData = new ObjectMetadata();
             metaData.setContentType(file.getContentType());
             metaData.setContentLength(file.getSize());
-            amazonS3Client.putObject(bucket, fileOriginalName, file.getInputStream(), metaData);
+            amazonS3.putObject(bucket, uuidFileName, file.getInputStream(), metaData);
             return ResponseEntity.ok(url);
         }catch(Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
